6d59ce8f77848def1a5950894412b8e60a54b16e
vishwaspuri <vishwaspuri09@gmail.com>
2020-04-17 13:55:59 +0530

    Enabled CORS

--- DocApp/DocApp/settings.py
+++ DocApp/DocApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'r!aq$iuiy=0aui%8&kn^obui*1ofisfnh&$4g@&-ft%m0$r&1d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'main',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'DocApp.urls'
 
 TEMPLATES = [
